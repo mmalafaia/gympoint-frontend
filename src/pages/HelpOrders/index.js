b48d6c31c533ce07b5dd857f66a3ecd186d2fb17
Manoel Malafaia <mmalafaia@gmail.com>
2019-12-26 18:02:01 -0300

    Partial work on HelpOrder

--- src/pages/HelpOrders/index.js
+++ src/pages/HelpOrders/index.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import api from '~/services/api';
 
-import { Container, Time } from './styles';
+import { Container, ButtonEdit } from './styles';
 
 export default function Students() {
+  const [helpOrderList, setHelpOrderList] = useState([]);
+
+  useEffect(() => {
+    async function loadStudents() {
+      const response = await api.get('help-orders');
+
+      setHelpOrderList(response.data);
+    }
+
+    loadStudents();
+  }, []);
+
   return (
     <Container>
       <header>
-        <h1>Gerenciando alunos</h1>
-        <button type="button">CADASTRAR</button>
-        <input placeholder="Buscar aluno" />
+        <h1>Pedidos de auxílio</h1>
       </header>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>ALUNO</th>
+              <th width="100"> </th>
+            </tr>
+          </thead>
+          <tbody>
+            {helpOrderList.map(hp => (
+              <tr>
+                <td>{hp.student.name}</td>
+                <td>
+                  <ButtonEdit type="button" onClick={() => {}}>
+                    responder
+                  </ButtonEdit>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </Container>
   );
 }
